62858fe48a2b397123f2e6f3fe073a8b3ffc88e8
giovanni-mendez <164027523+giovanni-mendez@users.noreply.github.com>
2025-05-21 15:10:24 -0300

    commit
    diagrama

--- TP_TitanesPacifico/obj/Debug/net9.0/TP-TitanesPacifico.AssemblyInfo.cs
+++ TP_TitanesPacifico/obj/Debug/net9.0/TP-TitanesPacifico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-TitanesPacifico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875025870203905c46fdeb1c09b52e372710e6e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-TitanesPacifico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-TitanesPacifico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
